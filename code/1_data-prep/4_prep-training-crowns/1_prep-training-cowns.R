d7a2eec311ed162c09767bcc847dedb9d7af7752
Derek Young <22568494+youngdjn@users.noreply.github.com>
2024-06-29 17:47:29 +0000

    Add training crows that include a SNAG class

--- code/1_data-prep/4_prep-training-crowns/1_prep-training-cowns.R
+++ code/1_data-prep/4_prep-training-crowns/1_prep-training-cowns.R
@@ -3,7 +3,8 @@
 # and merge PIPO and PIJE into PIPJ
 
 PREDICTED_TREECROWNS_W_FIELD_DATA_FILE = "/ofo-share/str-disp_drone-data-v2/predicted-treecrowns-w-field-data/predicted-treecrowns-w-field-data.gpkg"
-PREDICTED_TREECROWNS_W_FIELD_DATA_FILTERED_FILE = "/ofo-share/str-disp_drone-data-v2/predicted-treecrowns-w-field-data/predicted-treecrowns-w-field-data-filtered.gpkg"
+PREDICTED_TREECROWNS_W_FIELD_DATA_CLEANED_FILE = "/ofo-share/str-disp_drone-data-v2/predicted-treecrowns-w-field-data/predicted-treecrowns-w-field-data-cleaned.gpkg"
+PREDICTED_TREECROWNS_W_FIELD_DATA_FILTERED_FILE = "/ofo-share/str-disp_drone-data-v2/predicted-treecrowns-w-field-data/predicted-treecrowns-w-field-data-cleaned-snagsremoved.gpkg"
 
 
 library(tidyverse)
@@ -11,11 +12,18 @@ library(sf)
 
 crowns = st_read(PREDICTED_TREECROWNS_W_FIELD_DATA_FILE)
 
+# merge species that are functionally the same, and drop species that are very rare
 crowns = crowns |>
-  filter(percent_green_observed >= 50) |>
   filter(!(species_observed %in% c("QUEV"))) |>
   mutate(species_observed = recode(species_observed,
                                    "PIPO" = "PIPJ",
-                                   "PIJE" = "PIPJ"))
+                                   "PIJE" = "PIPJ")) |>
+  mutate(species_observed = ifelse(percent_green_observed < 10, "SNAG", species_observed))
+
+st_write(crowns, PREDICTED_TREECROWNS_W_FIELD_DATA_CLEANED_FILE, delete_dsn = TRUE)
+
+# drop snags (actually any trees with < 50% green crown volume)
+crowns = crowns |>
+  filter(percent_green_observed >= 50)
 
 st_write(crowns, PREDICTED_TREECROWNS_W_FIELD_DATA_FILTERED_FILE, delete_dsn = TRUE)

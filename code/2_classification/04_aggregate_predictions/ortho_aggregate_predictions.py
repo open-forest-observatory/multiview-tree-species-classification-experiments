33c97a8577c953d9ad98f7cefc3829dbeed48b82
David Russell <djrussell@ucdavis.edu>
2024-07-22 16:36:06 +0000

    Update ortho workflow to generate labeled polygons

--- code/2_classification/04_aggregate_predictions/ortho_aggregate_predictions.py
+++ code/2_classification/04_aggregate_predictions/ortho_aggregate_predictions.py
@@ -4,9 +4,10 @@ from pathlib import Path
 
 import geopandas as gpd
 import numpy as np
+from geograypher.constants import PRED_CLASS_ID_KEY
 from geograypher.predictors import assemble_tiled_predictions
+from geograypher.utils.files import ensure_containing_folder
 from geograypher.utils.geospatial import get_overlap_raster
-from geograypher.utils.prediction_metrics import compute_and_show_cf
 
 # Imports from the constants
 constants_dir = str(Path(Path(__file__).parent, "..").resolve())
@@ -15,14 +16,13 @@ from constants import (
     ALL_SITE_NAMES,
     DEFAULT_INPUT_DATA_DIR,
     DEFAULT_PREDICTION_DATA_DIR,
-    LABELS_COLUMN,
     get_aggregated_raster_pred_file,
-    get_figure_export_confusion_matrix_file,
     get_IDs_to_labels,
     get_labels_filename,
-    get_npy_export_confusion_matrix_file,
     get_ortho_filename,
+    get_predicted_labeled_polygons_file,
     get_prediction_folder,
+    get_unlabeled_crowns_file,
 )
 
 
@@ -32,6 +32,9 @@ def main(
     test_site,
     run_ID,
     training_sites,
+    full_site_prediction,
+    aggregate_raster,
+    label_polygons,
     include_snag_class,
 ):
 
@@ -52,63 +55,64 @@ def main(
         run_ID,
         prediction_data_dir=prediction_data_dir,
     )
-    # Where to save the confusion matrix figure
-    figure_export_confusion_matrix_file = get_figure_export_confusion_matrix_file(
+    predicted_labeled_polygons_file = get_predicted_labeled_polygons_file(
         test_site,
         training_sites=training_sites,
         mission_type="ortho",
         run_ID=run_ID,
         prediction_data_dir=prediction_data_dir,
     )
-    # Where to save the numpy representation of the confusion matrix
-    npy_export_confusion_matrix_file = get_npy_export_confusion_matrix_file(
-        test_site,
-        training_sites=training_sites,
-        mission_type="ortho",
-        run_ID=run_ID,
-        prediction_data_dir=prediction_data_dir,
-    )
+    # Get the appropriate polygons to label
+    if full_site_prediction:
+        unlabeled_polygons_file = get_unlabeled_crowns_file(
+            site=test_site, input_data_dir=input_data_dir
+        )
+    else:
+        unlabeled_polygons_file = get_labels_filename(
+            input_data_dir=input_data_dir, include_snag_class=include_snag_class
+        )
 
-    # Get the labels
-    labels_filename = get_labels_filename(
-        input_data_dir=input_data_dir, include_snag_class=include_snag_class
-    )
-    IDs_to_labels = get_IDs_to_labels(include_snag_class=include_snag_class)
-    num_classes = len(IDs_to_labels)
-    labels = list(IDs_to_labels.values())
+    # Aggregate the individual predictions to one raster with the most common class per pixel
+    if aggregate_raster:
+        # Get the labels
+        IDs_to_labels = get_IDs_to_labels(include_snag_class=include_snag_class)
+        num_classes = len(IDs_to_labels)
 
-    # List all the predicted files
-    pred_files = list(prediction_folder.glob("*"))
+        # List all the predicted files
+        pred_files = list(prediction_folder.glob("*"))
 
-    # Merge tiled predictions into one geospatial orthomosaic
-    assemble_tiled_predictions(
-        raster_input_file=ortho_filename,
-        pred_files=pred_files,
-        num_classes=num_classes,
-        class_savefile=aggregated_raster_pred_file,
-    )
-    # Label the polygons using the predicted raster
-    class_fractions, IDs_in_original = get_overlap_raster(
-        unlabeled_df=labels_filename,
-        classes_raster=aggregated_raster_pred_file,
-        num_classes=num_classes,
-    )
-    # Get the highest weight class prediction per polygon
-    pred_IDs = np.argmax(class_fractions, axis=1)
+        # Merge tiled predictions into one geospatial orthomosaic
+        assemble_tiled_predictions(
+            raster_input_file=ortho_filename,
+            pred_files=pred_files,
+            num_classes=num_classes,
+            class_savefile=aggregated_raster_pred_file,
+        )
 
-    # Turn this ID into a string label
-    pred_classes = [IDs_to_labels[pred_ID] for pred_ID in pred_IDs]
+    # Classify polygons
+    if label_polygons:
+        # Label the polygons using the predicted raster
+        class_fractions, polygon_IDs_in_original = get_overlap_raster(
+            unlabeled_df=unlabeled_polygons_file,
+            classes_raster=aggregated_raster_pred_file,
+            num_classes=num_classes,
+        )
+        # Get the highest weight class prediction per polygon
+        pred_IDs = np.argmax(class_fractions, axis=1)
 
-    # Read the ground truth classes for the corresponding polygons
-    gt_classes = gpd.read_file(labels_filename)[LABELS_COLUMN][IDs_in_original].tolist()
-    # Compute and show the confusion matrix
-    compute_and_show_cf(
-        pred_labels=pred_classes,
-        gt_labels=gt_classes,
-        labels=labels,
-        cf_plot_savefile=figure_export_confusion_matrix_file,
-        cf_np_savefile=npy_export_confusion_matrix_file,
-    )
+        # Turn this ID into a string label
+        pred_classes = [IDs_to_labels[pred_ID] for pred_ID in pred_IDs]
+
+        # Add the predicted classes to the dataframe
+        polygons = gpd.read(unlabeled_polygons_file)
+        # Create a new column for the predicted class and set all elements to unkownn
+        polygons[PRED_CLASS_ID_KEY] = "unknown"
+        # Set any polygons that we have a prediction for to the predicted class
+        polygons[PRED_CLASS_ID_KEY][polygon_IDs_in_original] = pred_classes
+        # Ensure that the export folder exists
+        ensure_containing_folder(predicted_labeled_polygons_file)
+        # Export the labeled polygons
+        polygons.to_file(predicted_labeled_polygons_file)
 
 
 def parse_args():
@@ -124,6 +128,21 @@ def parse_args():
         nargs="+",
         default=("00",),
         help="The run IDs to generate predictions from",
+    )
+    parser.add_argument(
+        "--full-site-prediction",
+        action="store_true",
+        help="Should predictions for the full site be generated",
+    )
+    parser.add_argument(
+        "--aggregate-raster",
+        action="store_true",
+        help="Run the step to aggregate individual chipped predictions to a single raster",
+    )
+    parser.add_argument(
+        "--label_polygons",
+        action="store_true",
+        help="Run the step to label polygons from the aggregated raster.",
     )
     parser.add_argument(
         "--input-data-dir",
@@ -156,5 +175,8 @@ if __name__ == "__main__":
                 test_site=test_site,
                 training_sites=ALL_SITE_NAMES,  # TODO update
                 run_ID=run_ID,
+                full_site_prediction=args.full_site_prediction,
+                aggregate_raster=args.aggregate_raster,
+                label_polygons=args.label_polygons,
                 include_snag_class=args.include_snag_class,
             )
